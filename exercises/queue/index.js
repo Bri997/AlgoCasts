028c79775cf864d76f1e53ab90f9fc11cab533ce
Bri997 <brian.musial@gmail.com>
2019-02-12 06:03:47 -0800

    queue weave

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,17 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    //data can be anything /name
+    this.data = [];
+  }
+  add(record) {
+    this.data.unshift(record);
+  }
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;

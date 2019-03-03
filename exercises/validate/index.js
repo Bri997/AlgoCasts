a4408d57edfd7160be0111894921cef43a90b516
Bri997 <brian.musial@gmail.com>
2019-03-03 15:48:44 -0800

    merge sort

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,25 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    /// min is null and max is node.data value
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    //move the value of the min for the right
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
